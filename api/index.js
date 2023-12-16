c3408eb331648c4d15f12d2fc9c4c27064e324f6
codermohit <iamthecodermohit@gmail.com>
2023-12-16 18:09:22 +0530

    feat : signin form submission

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
-import express from "express";
 import "dotenv/config";
 import "express-async-errors";
+
+import cors from "cors";
+import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -12,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,27 +1,44 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const res = await fetch("http://localhost:3000/api/auth/signup", formData);
+    const data = await res.json();
+    console.log(data);
+  }
+
+  function handleChange(e) {
+    setFormData((formData) => ({ ...formData, [e.target.id]: e.target.value }));
+  }
+
   return (
     <div className="p-3 max-w-sm mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign up</h1>
-      <form className="flex flex-col gap-4 ">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="Email"
           id="email"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           Sign up
