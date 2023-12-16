e0f74954202dd4233c96184c9859d592800f759b
codermohit <iamthecodermohit@gmail.com>
2023-12-16 05:59:02 +0530

    first commit

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,5 @@
+function Profile() {
+  return <div>Profile page</div>;
+}
+
+export default Profile;
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,5 @@
+function SignIn() {
+  return <div>SignIn Page</div>;
+}
+
+export default SignIn;
