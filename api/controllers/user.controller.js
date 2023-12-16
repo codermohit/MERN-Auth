bced72cb8fd57dcaec16fc2cb814f6e686d2f6b0
codermohit <iamthecodermohit@gmail.com>
2023-12-16 07:25:47 +0530

    Add : Routes and Controllers

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = async (req, res) => {
+  res.json({ msg: "Everthing in control" });
+};
--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
-
+import userRoutes from "./routes/user.route.js";
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, async () => {
   try {
