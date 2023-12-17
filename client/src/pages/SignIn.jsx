ee8e1e7a9d69b79f6a898252f6f3993d45b4e8ec
codermohit <iamthecodermohit@gmail.com>
2023-12-17 08:15:54 +0530

    refactor : uses redux store to handle state

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useUser } from "../redux/user/useUser.js";
 
 function SignIn() {
   const [formData, setFormData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState({ error: false, errorMsg: "" });
+  const {
+    loading,
+    error: { isError, errorMsg },
+    signInStart,
+    signInFailure,
+    signInSuccess,
+  } = useUser();
   const navigate = useNavigate();
 
   function handleChange(e) {
@@ -17,8 +23,7 @@ function SignIn() {
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      setLoading(true);
-      setError((err) => ({ ...err, error: false, errorMsg: "" }));
+      signInStart();
       console.log(formData);
       const res = await fetch("/api/auth/signin", {
         method: "POST",
@@ -32,12 +37,12 @@ function SignIn() {
         throw new Error(errorData?.errorMsg || "Something went wrong!");
       }
       const data = await res.json();
+      if (!data) throw new Error("No data found for the request");
+      signInSuccess(data);
       navigate("/");
-      console.log(data);
     } catch (error) {
-      setError((err) => ({ ...err, error: true, errorMsg: error.message }));
-    } finally {
-      setLoading(false);
+      const err = { isError: true, errorMsg: error.message };
+      signInFailure(err);
     }
   }
 
@@ -46,14 +51,14 @@ function SignIn() {
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
         <input
-          type="text"
+          type="email"
           placeholder="Email"
           id="email"
           className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
         />
         <input
-          type="text"
+          type="password"
           placeholder="Password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
@@ -66,7 +71,7 @@ function SignIn() {
         >
           Sign In
         </button>
-        {error.error && <p className="text-red-500">{error.errorMsg}</p>}
+        {isError && <p className="text-red-500">{errorMsg}</p>}
       </form>
       <div className="flex gap-2 mt-5">
         <p>Don&#39;t have an account?</p>
