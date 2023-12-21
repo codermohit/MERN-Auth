bd0b1ad3275ea18020019eac8cbc5c009195a19a
codermohit <iamthecodermohit@gmail.com>
2023-12-21 06:06:52 +0530

    add : upload image functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,83 @@
+import { useEffect, useRef, useState } from "react";
 import { useUser } from "../redux/user/useUser";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase.js";
 
 function Profile() {
   const { currentUser } = useUser();
+  const [image, setImage] = useState(undefined);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [uploadError, setUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const fileRef = useRef(false);
+
+  const handleUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = Date.now() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(Math.round(progress));
+      },
+      (error) => {
+        setUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData((formData) => ({
+            ...formData,
+            profilePicture: downloadURL,
+          }));
+        });
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (image) {
+      handleUpload(image);
+    }
+  }, [image]);
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
         <img
-          src={currentUser.profilePicture}
+          src={formData.profilePicture || currentUser.profilePicture}
           alt="profile pic"
           className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2"
+          onClick={() => fileRef.current?.click()}
         />
+        <p className="text-center">
+          {console.log(uploadProgress)}
+          {uploadError && (
+            <span className="text-red-700">Error uploading file</span>
+          )}
+          {uploadProgress > 0 && uploadProgress < 100 ? (
+            <span className="text-slate-700">{`Uploading : ${uploadProgress}%`}</span>
+          ) : uploadProgress === 100 ? (
+            <span className="text-green-700">Image uploaded successfully</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           type="text"
           id="username"
